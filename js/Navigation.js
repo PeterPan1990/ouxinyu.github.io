c879cdaef295ee7d623e4f6c750105ba16c0d1f6
ouxinyu <ouxinyu@hust.edu.cn>
2014-09-06 23:04:23 +0800

    0

--- js/Navigation.js
+++ js/Navigation.js
@@ -1,11 +1,11 @@
 ﻿document.write("<div id=\"Navi\" style=\"width:100%;height:30px;line-height:30px;\">");
 document.write("<ul style=\"list-style:none;height:30px;line-height:30px;border-bottom:1px solid gray;border-top:1px solid gray;\">");
-document.write("<li style=\"display:inline;height:30px;line-height:30px;padding:0 10px 0 0;\"><a href=\"../index.html\" style=\"font-size:14px;font-family:Calibri;\">主页</a></li>");
-document.write("<li style=\"display:inline;height:30px;line-height:30px;padding:0 10px 0 10px;\"><a href=\"../Course.html\" style=\"font-size:14px;font-family:Calibri;\">教学</a></li>");
-document.write("<li style=\"display:inline;height:30px;line-height:30px;padding:0 10px 0 10px;\"><a href=\"../Publication.html\" style=\"font-size:14px;font-family:Calibri;\">学术论文</a></li>");
-document.write("<li style=\"display:inline;height:30px;line-height:30px;padding:0 10px 0 10px;\"><a href=\"../Project.html\" style=\"font-size:14px;font-family:Calibri;\">科研项目</a></li>");
-document.write("<li style=\"display:inline;height:30px;line-height:30px;padding:0 10px 0 10px;\"><a href=\"../Award.html\" style=\"font-size:14px;font-family:Calibri;\">获奖</a></li>");
-document.write("<li style=\"display:inline;height:30px;line-height:30px;padding:0 10px 0 10px;\"><a href=\"../Links.html\" style=\"font-size:14px;font-family:Calibri;\">链接</a></li>");
-document.write("<li style=\"display:inline;height:30px;line-height:30px;padding:0 10px 0 10px;\"><a href=\"../AboutMe.html\" style=\"font-size:14px;font-family:Calibri;\">关于我</a></li>");
+document.write("<li style=\"display:inline;height:30px;line-height:30px;padding:0 10px 0 0;\"><a href=\"../index.html\" style=\"font-size:12px;font-family:Calibri;\">主页</a></li>");
+document.write("<li style=\"display:inline;height:30px;line-height:30px;padding:0 10px 0 10px;\"><a href=\"../Course.html\" style=\"font-size:12px;font-family:Calibri;\">教学</a></li>");
+document.write("<li style=\"display:inline;height:30px;line-height:30px;padding:0 10px 0 10px;\"><a href=\"../Publication.html\" style=\"font-size:12px;font-family:Calibri;\">学术论文</a></li>");
+document.write("<li style=\"display:inline;height:30px;line-height:30px;padding:0 10px 0 10px;\"><a href=\"../Project.html\" style=\"font-size:12px;font-family:Calibri;\">科研项目</a></li>");
+document.write("<li style=\"display:inline;height:30px;line-height:30px;padding:0 10px 0 10px;\"><a href=\"../Award.html\" style=\"font-size:12px;font-family:Calibri;\">获奖</a></li>");
+document.write("<li style=\"display:inline;height:30px;line-height:30px;padding:0 10px 0 10px;\"><a href=\"../Links.html\" style=\"font-size:12px;font-family:Calibri;\">链接</a></li>");
+document.write("<li style=\"display:inline;height:30px;line-height:30px;padding:0 10px 0 10px;\"><a href=\"../AboutMe.html\" style=\"font-size:12px;font-family:Calibri;\">关于我</a></li>");
 document.write("</ul>");
 document.write("</div>");
