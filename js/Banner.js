45a512b82830165d21cff6de9048dc87e479cbb5
ouxinyu <ouxinyu@hust.edu.cn>
2014-09-06 14:46:05 +0800

    2

--- js/Banner.js
+++ js/Banner.js
@@ -1,5 +1,5 @@
 ﻿document.write("<div id=\"Logo\" style=\"width:40%;height:60px;float:left;text-align:center;\">");
-document.write("<div style=\"width:100%;height:40px;line-height:40px;font-size:28px;\">欧老师的网站</div>");
+document.write("<div style=\"width:100%;height:40px;line-height:40px;font-size:28px;\">欧老师工作室</div>");
 document.write("<div style=\"width:100%;height:20px;line-height:20px;font-size:12px;color:gray;\">教育的根是苦的,但其果是甜的</div>");
 document.write("</div>");
 document.write("<div id=\"Navi\" style=\"width:60%;height:60px;float:left;\">");
@@ -9,6 +9,7 @@ document.write("<li style=\"display:inline;padding:0 5px 0 5px;\"><a href=\"../C
 document.write("<li style=\"display:inline;padding:0 5px 0 5px;\"><a href=\"../Publication.html\">Publication</a></li>");
 document.write("<li style=\"display:inline;padding:0 5px 0 5px;\"><a href=\"../Project.html\">Project</a></li>");
 document.write("<li style=\"display:inline;padding:0 5px 0 5px;\"><a href=\"../Award.html\">Award</a></li>");
+document.write("<li style=\"display:inline;padding:0 5px 0 5px;\"><a href=\"../Miscellaneous.html\">Miscellaneous</a></li>");
 document.write("<li style=\"display:inline;padding:0 5px 0 5px;\"><a href=\"../AboutMe.html\">AboutMe</a></li>");
 document.write("</ul>");
 document.write("</div>");
