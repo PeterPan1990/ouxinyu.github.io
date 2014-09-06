7bc798d18bb7108983195079033eb46d3ccec65c
ouxinyu <ouxinyu@hust.edu.cn>
2014-09-06 23:16:45 +0800

    0

--- js/Copyright.js
+++ js/Copyright.js
@@ -1,3 +1,3 @@
-﻿document.write("<div id=\"Bottom\" style=\"height:60px;line-height:60px;text-align:center;width:100%;border-top:1px solid gray;\">");
+﻿document.write("<div id=\"Bottom\" style=\"height:30px;line-height:30px;text-align:center;width:100%;border-top:1px solid gray;\">");
 document.write("Copyright © 2014-2020, OuXinyu, All Rights Reserved");
 document.write("</div>");
