86333c03e4e0e1daf00bbfbb8beb555e1135b83b
ouxinyu <ouxinyu@hust.edu.cn>
2014-09-07 10:02:22 +0800

    cnzz

--- js/Copyright.js
+++ js/Copyright.js
@@ -1,3 +1,12 @@
-﻿document.write("<div id=\"Bottom\" style=\"height:30px;line-height:30px;text-align:center;width:100%;border-top:1px solid gray;\">");
+﻿document.write("<div id=\"Bottom\" style=\"height:40px;line-height:40px;text-align:center;width:100%;border-top:1px solid gray;\">");
+
+document.write("<div id=\"Copyright\" style=\"height:20px;line-height:20px;\">");
 document.write("Copyright © 2014-2020, OuXinyu, All Rights Reserved");
+document.write("</div>");
+
+document.write("<div id=\"cnzz\" style=\"height:20px;line-height:20px;\">");
+var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cspan id='cnzz_stat_icon_1253214442'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s5.cnzz.com/z_stat.php%3Fid%3D1253214442%26online%3D1%26show%3Dline' type='text/javascript'%3E%3C/script%3E"));
+document.write("</div>");
+
 document.write("</div>");
