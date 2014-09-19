87e93080a3dc8b01a5c59030b41864c230eb8515
shiorioxy <shiorioxy@qq.com>
2014-09-20 01:17:23 +0800

    1

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,8 +1,8 @@
 ﻿document.write("<div class=\"sidebar\">");
 document.write("<h3>Latest</h3>");
-document.write("<p><a href=\"#\">Caffe + Ubuntu 14.04 + CUDA 6.5 新手安装配置指南</a> <i style=\"color:gray;\">[2014-07-23]</i></p>");
-document.write("<p><a href=\"#\">Caffe配置指南 on Windows8.1 + CUDA6.5 + VS2013 </a> <i style=\"color:gray;\">[2014-07-19]</i></p>");
-document.write("<p><a href=\"#\">Cuda-convnet配置指南 on Windows8.1 + CUDA6.5 + VS2013</a> <i style=\"color:gray;\">[2014-07-17]</i></p>");
+document.write("<p><a href=\"Blogs\\20140723001.html\">Caffe + Ubuntu 14.04 + CUDA 6.5 新手安装配置指南</a> <i style=\"color:gray;\">[2014-07-23]</i></p>");
+document.write("<p><a href=\"Blogs\\20140723001.html\">Caffe配置指南 on Windows8.1 + CUDA6.5 + VS2013 </a> <i style=\"color:gray;\">[2014-07-19]</i></p>");
+document.write("<p><a href=\"Blogs\\20140723001.html\">Cuda-convnet配置指南 on Windows8.1 + CUDA6.5 + VS2013</a> <i style=\"color:gray;\">[2014-07-17]</i></p>");
 document.write("<h3>Contact</h3>");
 document.write("<ul>");
 document.write("<li>E—Mail：shiorioxy@qq.com</li>");
