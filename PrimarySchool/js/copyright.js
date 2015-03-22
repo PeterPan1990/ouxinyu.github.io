f0642ac5d85bb85d131cf1d4074296b5400033cd
Xinyu Ou <ouxinyu@hust.edu.cn>
2015-03-22 14:08:13 +0800

    1

--- PrimarySchool/js/copyright.js
+++
@@ -1,6 +0,0 @@
-﻿document.write("<div id=\"footer\"></div>");
-document.write("<div class=\"indent\">");
-document.write("<div id=\"Copyright\" class=\"fleft\">");
-document.write("Copyright &copy; Xin-Yu Ou （欧新宇） | All Rights Reserved");
-document.write("</div>");
-document.write("</div>");
