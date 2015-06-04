edd68f563111891326646804b56a16556834d2ba
Xinyu Ou <ouxinyu@hust.edu.cn>
2015-06-04 22:04:56 +0800

    1

--- js/sidebar_content .js
+++ js/sidebar_content .js
@@ -1,9 +1,4 @@
 ﻿document.write("<div class=\"sidebar\">");
-
-document.write("<div style=\"text-align:left;width:100%;\">");
-document.write("<iframe name=\"weather_inc\" src=\"http://i.tianqi.com/index.php?c=code&id=7\" width=\"220\" height=\"90\" frameborder=\"0\" marginwidth=\"0\" marginheight=\"0\" scrolling=\"no\"></iframe>");
-document.write("</div>");
-
 document.write("<h3>Latest</h3>");
 document.write("<p><a href=\"Blogs\\20140723001.html\">Caffe + Ubuntu 15.04 + CUDA 7.0 新手安装配置指南</a> <i style=\"color:gray;\">[2015-05-06]</i></p>");
 document.write("<p><a href=\"Blogs\\20150413001.html\">SPP-net + Windows 8.1 安装配置指南</a> <i style=\"color:gray;\">[2015-04-13]</i></p>")
@@ -16,6 +11,5 @@ document.write("</ul>");
 document.write("<h3>My Links</h3>");
 document.write("<ul>");
 document.write("<li><a href=\"http://www.scholarmate.com/profile/xinyuou1 \" target=\"_blank\"> ScholarMate</a></li>");
-document.write("<li><a href=\"PrimarySchool/home.html\" target=\"_blank\"> 明通小学杨老师班</a></li>");
 document.write("</ul>");
 document.write("</div>");
