b9004f6086a3240c5d5b1fb1103b4b8d03bec697
Xinyu Ou <ouxinyu@hust.edu.cn>
2015-06-04 22:00:35 +0800

    0

--- js/sidebar.js
+++ js/sidebar.js
@@ -7,7 +7,7 @@ document.write("</div>");
 document.write("<h3>Latest</h3>");
 document.write("<p><a href=\"Blogs\\20140723001.html\">Caffe + Ubuntu 15.04 + CUDA 7.0 新手安装配置指南</a> <i style=\"color:gray;\">[2015-05-06]</i></p>");
 document.write("<p><a href=\"Blogs\\20150413001.html\">SPP-net + Windows 8.1 安装配置指南</a> <i style=\"color:gray;\">[2015-04-13]</i></p>")
-document.write("<p><a href=\"http:\\nbviewer.ipython.org\\github\\ouxinyu\\ouxinyu.github.io\\blob\\master\\Blogs\\iPythonNoteBook.ipynb\">Windows8.1 下安装iPython NoteBook 平台 </a> <i style=\"color:gray;\">[2015-06-04]</i></p>");;
+document.write("<p><a href=\"http:\nbviewer.ipython.org\github\ouxinyu\ouxinyu.github.io\blob\master\Blogs\iPythonNoteBook.ipynb\">Windows8.1 下安装iPython NoteBook 平台 </a> <i style=\"color:gray;\">[2015-06-04]</i></p>");;
 document.write("<h3>Contact</h3>");
 document.write("<ul>");
 document.write("<li>E—Mail：shiorioxy@qq.com</li>");
